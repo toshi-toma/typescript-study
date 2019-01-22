d2c532eff7e2d3eda3f230f05fd337580f696f1d
toshi-toma <toshitoma1217@gmail.com>
2019-01-22 23:50:02 +0900

    classを追加

--- src/greeter.js
+++ src/greeter.js
@@ -1,5 +1,15 @@
+var Student = /** @class */ (function () {
+    // the use of public on arguments to the constructor is a shorthand that allows us to automatically create properties with that name.
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
-var user = { firstName: "Jane", lastName: "User" };
+var user = new Student("Jane", "M.", "User");
 document.body.innerHTML = greeter(user);
--- src/greeter.ts
+++ src/greeter.ts
@@ -1,3 +1,11 @@
+class Student {
+  fullName: string;
+  // the use of public on arguments to the constructor is a shorthand that allows us to automatically create properties with that name.
+  constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
+
 interface Person {
   firstName: string;
   lastName: string;
@@ -7,6 +15,6 @@ function greeter(person: Person) {
   return "Hello, " + person.firstName + " " + person.lastName;
 }
 
-let user = { firstName: "Jane", lastName: "User" };
+let user = new Student("Jane", "M.", "User");
 
 document.body.innerHTML = greeter(user);
